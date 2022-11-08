5aa877be4d4c963dee9906e8e9f4edd71f2065b2
Matthieu Mary <matthieu.mary2@gmail.com>
2022-11-08 11:06:19 +0100

    Improve error messages on modal

--- scripts/factories/MediasFactory.js
+++ scripts/factories/MediasFactory.js
@@ -1,13 +1,16 @@
 const MediaFactory = (data, photographer) => {
-    const { id, title, image, video, likes, date, price } = data;
+    const { id, title, image, video, likes } = data;
+    
+    const { name, price } = photographer //!!!!!!!!!Photographer est appelé autant de fois qu'il y a de médias ... bug a résoudre!!!!!!!!!!!!!
 
     // Get only photographers first name to get the right media images
-    const photographerName = photographer.name;
+    const photographerName = name;
     const fullNameArr = photographerName.split(" ");
-    const firstName = fullNameArr[0]
+    const firstName = fullNameArr[0];
 
     const picture = `../assets/SamplePhotos/${firstName}/${image}`
     const mediasSection = document.querySelector(".medias");
+    const heart = "../assets/icons/Vector.svg"
     
     function createMediaCard() {
         const card = document.createElement("div");
@@ -18,11 +21,11 @@ const MediaFactory = (data, photographer) => {
         mediaInfos.classList.add("media-infos");
         const h3 = document.createElement("h3");
         h3.textContent = title;
-        const toto = document.createElement("span");
-        toto.textContent = likes;
+        const cardLikes = document.createElement("span");
+        cardLikes.textContent = `${likes} coeurs`;
         
         mediaInfos.appendChild(h3)
-        mediaInfos.appendChild(toto)
+        mediaInfos.appendChild(cardLikes)
 
         card.appendChild(img)
         card.appendChild(mediaInfos)
@@ -31,5 +34,23 @@ const MediaFactory = (data, photographer) => {
 
         return { mediasSection }
     }
-    return { createMediaCard }
+
+    function createEncart() {
+
+        const encart = document.querySelector(".likes-and-price");
+        let totalLikes = 23999;
+
+        const p = document.querySelector("p");
+        p.textContent = totalLikes;
+        const span = document.createElement("span");
+        span.textContent = `${price}€ / jour`;
+        encart.appendChild(p);
+        encart.appendChild(span);
+
+        return { encart }
+
+    }
+    createEncart()
+    return { createMediaCard, createEncart }
+    
 }   
