313e8b59b4a3e2b956176c520e0a96039d3b9345
Matthieu Mary <matthieu.mary2@gmail.com>
2022-11-28 18:30:05 +0100

    modify image and video logic on lightBox

--- scripts/utils/lightBox.js
+++ scripts/utils/lightBox.js
@@ -16,7 +16,7 @@ function lightBox(e) {
   lightbox.dataset.key = indexMedia;
   lightbox.classList.add("active");
 
-    createLightBox(lightbox);
+  createLightBox(lightbox);
 }
 
 function createLightBox(lightbox) {
@@ -24,23 +24,35 @@ function createLightBox(lightbox) {
   const allMedias = getAllMedias();
   const currentKey = lightbox.dataset.key;
   const currentImage = allMedias[currentKey].querySelector("img");
-  // const currentVideo = allMedias[currentKey].querySelector("video");
-  // console.log(currentVideo)
-  const currentTitle = allMedias[currentKey].querySelector("h3").textContent;
-  
-  // IMAGES
   const imageLightBox = document.querySelector(".lightbox-img");
-  imageLightBox.setAttribute("src", currentImage.src);
-  
-  // VIDEO
-  // const videoLightBox = document.createElement("video");
-  // videoLightBox.classList.add("lightbox-video");
-  // videoLightBox.setAttribute("src", currentVideo);
-  
+  const currentVideo = allMedias[currentKey].querySelector("video");
+  const mediaVideo = document.createElement("video");
+  const currentTitle = allMedias[currentKey].querySelector("h3").textContent;
+
+  console.log()
+  // IMAGES OR VIDEO
+  if (allMedias[currentKey].firstChild.src.includes(".jpg")) {
+    imageLightBox.setAttribute("src", currentImage.src);
+  } else {
+    imageLightBox.replaceWith(mediaVideo);
+    mediaVideo.classList.add("lightbox-video");
+    mediaVideo.setAttribute("src", currentVideo.src);
+    mediaVideo.setAttribute("controls", true);
+    const mediaVideoKey = mediaVideo.parentNode.parentNode.dataset.key;
+    console.log(currentKey);
+    if (mediaVideoKey !== currentKey) {
+      const mediaImage = document.createElement("img");
+      mediaImage.classList.add("lightbox-img");
+      mediaImage.setAttribute("src", currentImage.src);
+      mediaVideo.replaceWith(mediaImage);
+      console.log(mediaImage);
+    }
+  }
+
   // CLOSE BUTTON
   const closeBtn = document.querySelector(".lightbox-close");
   closeBtn.addEventListener("click", () => closeLightBox(lightbox));
-  
+
   //   TITLE
   const title = document.querySelector(".lightbox h3");
   title.textContent = currentTitle;
@@ -49,17 +61,17 @@ function createLightBox(lightbox) {
   function toggleChevronStyle() {
     const chevronLeft = document.querySelector(".chevron-left");
     const chevronRight = document.querySelector(".chevron-right");
-    if (Number(currentKey) === allMedias.length - 1 ) {
+    if (Number(currentKey) === allMedias.length - 1) {
       chevronRight.classList.add("inactive");
     } else if (Number(currentKey) === 0) {
       chevronLeft.classList.add("inactive");
     } else {
-      chevronRight.classList.remove("inactive")
-      chevronLeft.classList.remove("inactive")
+      chevronRight.classList.remove("inactive");
+      chevronLeft.classList.remove("inactive");
     }
   }
   toggleChevronStyle();
-  
+
   return { lightbox };
 }
 
@@ -72,11 +84,11 @@ function closeLightBox(lightbox) {
 function previousImage() {
   const lightbox = document.querySelector(".lightbox");
   let currentKey = parseInt(lightbox.dataset.key);
-  if(currentKey > 0) {
+  if (currentKey > 0) {
     currentKey -= 1;
     lightbox.dataset.key = currentKey;
     createLightBox(lightbox);
-  } 
+  }
 }
 
 function nextImage() {
